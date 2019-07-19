62bb450b84ec40fc580520b058fe3935611f6592
yguney <yguney@sabanciuniv.edu>
2019-07-19 21:26:10 +0300

    Add existing file

--- pages/_app.js
+++ pages/_app.js
@@ -14,8 +14,16 @@ import '../styles/ConnectionTimerWindow.css'; // import css
 import '../styles/ConnectionTimerWindow.css'; // import css
 import '../styles/Desktop.css'
 class MyApp extends App {
+  constructor(props) {
+    super(props);
 
-  
+
+
+    this.state = { 
+   show:false
+    };
+  }  
+
   static async getInitialProps({ Component, ctx }) {
     let pageProps = {}
 
@@ -25,36 +33,42 @@ class MyApp extends App {
 
     return { pageProps }
   }
-
+componentDidMount(){
+  this.setState({show:true})
+}
   render() {
     const { Component, pageProps } = this.props
+if(this.state.show == true){
+  return(
+    <Container>
 
-    return (
-      <Container>
+    <Component {...pageProps} />
+    <style jsx global>{`
+      .page-transition-enter {
+        opacity: 0.01;
+        transform: scale(1.1);        }
+      .page-transition-enter-active {
+        opacity: 1;
+        transform: scale(1);
+        transition: all 1000ms;
+      }
+      .page-transition-exit {
+        opacity: 1;
+        transform: scale(1);
+    }
+      .page-transition-exit-active {
+        opacity: 0.01;
+        transform: scale(1.1);
+        transition: all 1000ms;
 
-        <Component {...pageProps} />
-        <style jsx global>{`
-          .page-transition-enter {
-            opacity: 0.01;
-            transform: scale(1.1);        }
-          .page-transition-enter-active {
-            opacity: 1;
-            transform: scale(1);
-            transition: all 1000ms;
-          }
-          .page-transition-exit {
-            opacity: 1;
-            transform: scale(1);
-        }
-          .page-transition-exit-active {
-            opacity: 0.01;
-            transform: scale(1.1);
-            transition: all 1000ms;
-
-          }
-        `}</style>
-      </Container>
-    )
+      }
+    `}</style>
+  </Container>
+  )
+}else{
+  return(null)
+}
+    
   }
 }
 
